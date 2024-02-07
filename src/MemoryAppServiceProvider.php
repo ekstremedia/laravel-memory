224a541e3435d66a3a5b3ef51485a369ab79c60e
Terje Nesthus, working from home <terje@fdvhuset.no>
2024-02-07 17:10:17 +0100

    Initial commit

---
+++ src/MemoryAppServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Ekstremedia\MemoryApp;
+
+use Illuminate\Support\ServiceProvider;
+
+class MemoryAppServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Bootstrapping code here, such as routes, views, migrations, etc.
+    }
+
+    public function register()
+    {
+        // Register bindings, configurations, etc.
+    }
+}
+
