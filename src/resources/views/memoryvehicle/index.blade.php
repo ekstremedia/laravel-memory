066317f0e4aa63ea76ae62c3b18221e2260d6bd9
Terje Nesthus, working from home <terje@fdvhuset.no>
2024-02-12 19:56:14 +0100

    Vehicle component and sidebar layout changes

--- src/resources/views/memoryvehicle/index.blade.php
+++ src/resources/views/memoryvehicle/index.blade.php
@@ -3,24 +3,22 @@
 @extends('memoryapp::layouts.app')
 
 @section('content')
-    <div class="w-full">
-            <h1 class="text-2xl font-semibold leading-tight">
-                {{ trans('memoryapp::messages.vehicles') }}
-            </h1>
-            <a href="{{ route('memory.vehicles.create') }}">
-                <button class="flex items-center space-x-2 text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="submit">
-                    <i class="fas fa-plus"></i>
-                    <i class="fa fa-car"></i>
-                    <span>
+    <div class="flex justify-between p-4 w-full">
+        <h1 class="text-2xl font-semibold leading-tight">
+            <i class="fa fa-car fa-fw"></i> {{ trans('memoryapp::messages.vehicles') }}
+        </h1>
+        <a href="{{ route('memory.vehicles.create') }}">
+            <button class="flex items-center space-x-2 text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="submit">
+                <i class="fas fa-plus"></i>
+                <i class="fa fa-car"></i>
+                <span>
                         {{ trans('memoryapp::messages.vehicle.create') }}
                     </span>
-                </button>
-            </a>
-
-
-        <div class="py-6 w-full">
-            @include('memoryapp::memoryvehicle.partials.vehicles_list', ['vehicles' => $vehicles])
-        </div>
+            </button>
+        </a>
+    </div>
+    <div class="">
+        @include('memoryapp::memoryvehicle.partials.vehicles_list', ['vehicles' => $vehicles])
     </div>
 @endsection
--- src/resources/views/memoryvehicle/partials/vehicle.blade.php
+++ src/resources/views/memoryvehicle/partials/vehicle.blade.php
@@ -1,29 +1,34 @@
 {{--{{ $vehicle }}--}}
-<div class="bg-white w-full shadow-xl sm:rounded-lg p-5 ">
-    <div class="flex justify-start space-x-4">
-        <div class="text-2xl">
-            <a href="{{ route('memory.vehicles.show', $vehicle->uuid) }}"
-               class="text-blue-600 hover:text-blue-900">
-                {{ $vehicle->brand }} {{ $vehicle->model }}
-            </a>
-            <span class="text-sm">
-            {{ $vehicle->year }}
-        </span>
-        </div>
-        @include('memoryapp::memoryvehicle.partials.plate', ['plate' => $vehicle->plate_number])
-    </div>
-    <hr>
-    <a href="{{ route('memory.vehicles.edit', $vehicle) }}"
-       class="text-indigo-600 hover:text-indigo-900">{{ trans('memoryapp::messages.general.Edit') }}</a>
-    <div class="flex">
-        <div class="border px-4 py-2 bg-indigo-50 w-1/3 rounded-lg shadow">
-            <div>
-                Drivstoff
+<div class="mx-4 rounded-xl bg-white p-3 shadow-xl sm:rounded-lg">
+    <div class="flex items-center justify-between space-x-4">
+        <div class="flex w-full justify-between">
+            <div class="text-2xl">
+                <a href="{{ route('memory.vehicles.show', $vehicle->uuid) }}"
+                   class="text-blue-600 hover:text-blue-900">
+                    {{ $vehicle->brand }} {{ $vehicle->model }}
+                </a>
+                <span class="text-sm">
+                    {{ $vehicle->year }}
+                </span>
             </div>
-            <div>
+            <div class="">
+                @include('memoryapp::memoryvehicle.partials.plate', ['plate' => $vehicle->plate_number])
+            </div>
+        </div>
+        <a href="{{ route('memory.vehicles.edit', $vehicle) }}"
+           class="text-green-600 hover:text-indigo-900" title="{{ trans('memoryapp::messages.general.Edit') }}">
+            <i class="fa fa-edit fa-fw"></i>
+        </a>
+    </div>
+    <div class="flex flex-col rounded-lg border bg-indigo-50 px-2 py-2 shadow lg:w-1/3 space-y-2">
+        <h2 class="text-left text-blue-800 w-full bg-blue-100 px-2 py-1 rounded">
+            Drivstoff
+        </h2>
+        <div class="flex flex-row items-end">
+            <div class="flex w-full items-center justify-between">
                 @if($vehicle->latestFuel)
                     <table class="w-full">
-                        <thead class="text-left">
+                        <thead>
                         <tr>
                             <th>
                                 Date
@@ -34,12 +39,9 @@
                             <th>
                                 Total sum
                             </th>
-                            <th>
-
-                            </th>
                         </tr>
                         </thead>
-                        <tr>
+                        <tr class="text-center">
                             <td>
                                 <a href="{{ route('memory.vehicles.fuel.show', ['vehicle_uuid' => $vehicle->uuid, 'fuel' => $vehicle->latestFuel->uuid]) }}"
                                    class="text-blue-600 hover:text-blue-900">
@@ -58,13 +60,6 @@
                                     {{ $vehicle->latestFuel->total_price }}
                                 </a>
                             </td>
-                            <td>
-                                <a href="{{ route('memory.vehicles.fuel.create', ['vehicle_uuid' => $vehicle->uuid]) }}"
-                                   class="hover:text-indigo-900 bg-green-400 hover:bg-green-500 px-2 py-1 rounded text-green-900 shadow transition duration-300">
-                                    <i class="fa-solid fa-fw fa-gas-pump"></i>
-                                    {{ trans('memoryapp::messages.general.New') }}
-                                </a>
-                            </td>
                         </tr>
                     </table>
                 @else
@@ -72,8 +67,12 @@
                         {{ trans('memoryapp::messages.vehicle.fuel.no_fuel') }}
                     </div>
                 @endif
-
             </div>
+            <a href="{{ route('memory.vehicles.fuel.create', ['vehicle_uuid' => $vehicle->uuid]) }}"
+               class="whitespace-nowrap rounded bg-green-200 px-2 py-1 text-green-900 shadow transition duration-300 hover:bg-green-500 hover:text-indigo-900 border border-green-600/30 ">
+                <i class="fa-solid fa-fw fa-gas-pump"></i>
+                {{ trans('memoryapp::messages.general.New') }}
+            </a>
         </div>
     </div>
 </div>
